6b81e6f1fad4554bd574aa24bd8efb40dc8e3986
Aundra Miller <miller.aundra@gmail.com>
2016-04-18 15:49:50 -0700

    update html page and uncomment js click functions in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,15 @@
 $(document).ready(function() {
-  // $("h1").hover(function() {
-  //   alert("This is a header.");
-  // });
-  //
-  // $("p").click(function() {
-  //   alert("This is a paragraph.");
-  // });
-  //
-  // $("img").dblclick(function() {
-  //   alert("This is an image.");
-  // });
-  //
-  // $("h2").click(function() {
-  //   alert("This is a list");
-  // });
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("h4").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("h2").click(function() {
+    alert("This is a list");
+  });
 
   $("li").dblclick(function() {
     alert("This is an unordered list.");
